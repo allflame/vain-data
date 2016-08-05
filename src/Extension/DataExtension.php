7a31a45c921a4770060486533da414e41a45a37d
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-08-05 12:20:15 +0300

    Code style fixes

---
+++ src/Extension/DataExtension.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-data
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-data
+ */
+namespace Vain\Data\Extension;
+
+use Vain\Core\Extension\AbstractExtension;
+
+/**
+ * Class DataExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class DataExtension extends AbstractExtension
+{
+}
